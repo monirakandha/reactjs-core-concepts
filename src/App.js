a6036234a3bd6cd531cfa77321d88a5284540a87
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-16 09:49:49 +0600

    props add

--- src/App.js
+++ src/App.js
@@ -7,24 +7,36 @@ const singer2 = {name: 'Eva Rahaman' , job : 'Female Singer'}
 function App() {
   return (
     <div className="App">
-      <Person></Person>
-      <Person></Person>
-      <Friend></Friend>
+      <Person name="Rubel" nayik="Shabnur"></Person>
+      <Person name="Nayok BappRaz" nayik="Moushumi"></Person>
+      <Person name="Nayok BappRaz" nayik="Moushumi"></Person>
+      <Person name="Nayok Kuber" nayik="KoPila"></Person>
+      <Person name="Shakib Khan" nayik="Opu Bishawas"></Person>
+      <Person name="Jodu Modu" nayik="Solima"></Person>
+      <Friend name="Kodu" phone="0999"></Friend>
+      <Friend name="Modu" phone="016235"></Friend>
+      <Friend name="Jodu" phone="0125697"></Friend>
+      <Friend name="Lodu" phone="012369987456"></Friend>
+      <Friend name="Podu" phone="0899667"></Friend>
+      <Friend name="Oodu" phone="0987668"></Friend>
+      <Friend name="Eodu" phone="0325896654"></Friend>
     </div>
   );
 }
-function Person() {
+function Person(props) {
+  console.log(props); 
   return (<div className = "person">
-    <h1>Kaj Kore?</h1>
-  <p>Profession:Cricketer</p>
+    <h1>{props.name}</h1>
+  <h2>Nayika: {props.nayik}</h2>
   </div>);
 }
 export default App;
 
 
-function Friend () {
+function Friend (props) {
+  console.log(props);
   return (<div className = "person">
-  <h1>Kaj kormo Kor Kore?</h1>
-<p>Profession:Cricketer</p>
+  <h1>{props.name}</h1>
+<h2>{props.phone}</h2>
 </div>);
 }
