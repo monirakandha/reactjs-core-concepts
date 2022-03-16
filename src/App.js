1799badf148cf952b935fdfc70911450e28b4017
monirhosenakanda <monirhosenakanda@gmail.com>
2022-03-16 09:02:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+const number = 5555;
+const singer = {name : 'Dr. Mahfuz' , job:'SInger'}
+const singer2 = {name: 'Eva Rahaman' , job : 'Female Singer'}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person></Person>
+      <Person></Person>
+      <Friend></Friend>
     </div>
   );
 }
-
+function Person() {
+  return (<div className = "person">
+    <h1>Kaj Kore?</h1>
+  <p>Profession:Cricketer</p>
+  </div>);
+}
 export default App;
+
+
+function Friend () {
+  return (<div className = "person">
+  <h1>Kaj kormo Kor Kore?</h1>
+<p>Profession:Cricketer</p>
+</div>);
+}
